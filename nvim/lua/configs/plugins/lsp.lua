6a17ee86e2cbe3d7736d299475031b08b02933dc
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-02-11 15:50:53 +0530

    Fixed lsp keymaps

--- nvim/lua/configs/plugins/lsp.lua
+++ nvim/lua/configs/plugins/lsp.lua
@@ -39,14 +39,14 @@ return {
 				callback = function(event)
 					local opts = { buffer = event.buf }
 
-					vim.keymap.set("n", "K", "<cmd>vim.lsp.buf.hover()<cr>", opts)
-					vim.keymap.set("n", "gd", "<cmd>vim.lsp.buf.definition()<cr>", opts)
-					vim.keymap.set("n", "gD", "<cmd>vim.lsp.buf.declaration()<cr>", opts)
-					-- vim.keymap.set("n", "gi", "<cmd>vim.lsp.buf.implementation()<cr>", opts)
-					vim.keymap.set("n", "go", "<cmd>vim.lsp.buf.type_definition()<cr>", opts)
-					-- vim.keymap.set("n", "gr", "<cmd>vim.lsp.buf.references()<cr>", opts)
-					vim.keymap.set("n", "<F2>", "<cmd>vim.lsp.buf.rename()<cr>", opts)
-					vim.keymap.set("n", "<C-.>", "<cmd>vim.lsp.buf.code_actions()<cr>", opts)
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					-- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					-- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					vim.keymap.set("n", "<C-.>", "<cmd>lua vim.lsp.buf.code_actions()<cr>", opts)
 				end,
 			})
 
