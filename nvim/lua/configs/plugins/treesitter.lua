d663de08be93362fcc49386d52d7971c177ab943
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-03-06 12:35:22 +0530

    Added treesitter context config

--- nvim/lua/configs/plugins/treesitter.lua
+++ nvim/lua/configs/plugins/treesitter.lua
@@ -84,5 +84,11 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				max_lines = 3,
+				multiline_threshold = 1,
+			})
+		end,
 	},
 }
