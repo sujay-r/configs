bfe68f4df6d5b4312ad41d77fa04dd424c94fcb1
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-03-06 10:40:41 +0530

    Added mini.surround, change filepath in lualine to relative

--- nvim/lua/configs/plugins/lualine.lua
+++ nvim/lua/configs/plugins/lualine.lua
@@ -1,18 +1,18 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local lualine = require('lualine')
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
 
-        lualine.setup({
-            sections = {
-                lualine_a = {"mode"},
-                lualine_b = {"filename"},
-                lualine_c = {"branch", "diff"},
-                lualine_x = {"diagnostics"},
-                lualine_y = {"fileformat", "filetype"},
-                lualine_z = {"location"},
-            }
-        })
-    end
+		lualine.setup({
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { { "filename", path = 1 } },
+				lualine_c = { "branch", "diff" },
+				lualine_x = { "diagnostics" },
+				lualine_y = { "fileformat", "filetype" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
