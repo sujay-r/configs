ee091d52760c32eb7b4ac8a317e7ab03dfee0116
Sujay Rokade <sujay.rokade.2k@gmail.com>
2025-03-24 23:22:28 +0530

    Added highlight group for blink-cmp

--- nvim/lua/configs/plugins/lsp.lua
+++ nvim/lua/configs/plugins/lsp.lua
@@ -101,6 +101,17 @@ return {
 
 									return icon .. ctx.icon_gap
 								end,
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+
+									return hl
+								end,
 							},
 						},
 					},
